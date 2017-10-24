404ace3fb7c060b0c4f79397066a6f755bf64ddc
Thomas Desmond <thomasdes533@gmail.com>
2017-10-23 17:58:45 -0700

    Initial Commit of Prism Blank Template

---
+++ PrismBlankTemplate/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using PrismBlankTemplate.Views;
+using PrismBlankTemplate.Views;
+
+namespace PrismBlankTemplate
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
+
